cf0797bffa927118e4de1333765725acb0c4f4b1
Dwi Ayu Rimba <ayurimbadwi@gmail.com>
2025-08-16 10:57:10 +0700

    create index and other files

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Profile website loaded!");
+
+  // contoh interaktif kecil
+  const name = prompt("Siapa nama kamu?");
+  if (name) {
+    alert("Halo " + name + "! Selamat datang di profile saya 🚀");
+  }
+});
